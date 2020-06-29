c852259b5218e953b8b6c83eb8661f537c7ac4c8
the-neighbor <the-neighbor@posteo.net>
2020-06-28 20:08:26 -0400

    in step 9 we expanded upon the collision detection to check for solid tiles on each side of the bounding box and have them stop our lateral movement, jumping, and falling

---
+++ 9/Map.lua
@@ -0,0 +1,169 @@
+Map = Class{}
+
+TILE_BRICK = 1
+TILE_EMPTY = -1
+
+-- cloud tiles
+CLOUD_LEFT = 6
+CLOUD_RIGHT = 7
+
+-- bush tiles
+BUSH_LEFT = 2
+BUSH_RIGHT = 3
+
+-- mushroom tiles
+MUSHROOM_TOP = 10
+MUSHROOM_BOTTOM = 11
+
+-- jump block
+JUMP_BLOCK = 5
+JUMP_BLOCK_HIT = 9
+
+
+SCROLL_SPEED = 62
+
+require 'util'
+require 'Player'
+
+function Map:init()
+    self.spritesheet = love.graphics.newImage('graphics/spritesheet.png')
+    self.tileWidth = 16
+    self.tileHeight = 16
+    self.mapWidth = 30
+    self.mapHeight = 28
+    self.tiles = {}
+    self.gravity = 15
+    --track camera coordinates so we draw the right subsection of the map
+    self.camX = 0
+    self.camY = -3
+
+    self.tileSprites = generateQuads(self.spritesheet, self.tileWidth, self.tileHeight)
+
+    self.mapWidthPixels = self.mapWidth * self.tileWidth
+    self.mapHeightPixels = self.mapHeight * self.tileHeight
+
+   -- clear out map
+   for y = 1, self.mapHeight do
+       for x = 1, self.mapWidth do
+	   self:setTile(x, y, TILE_EMPTY)
+       end
+   end
+   -- generate map
+   self:generate()
+   -- add player
+   self.player = Player(self)
+
+end
+
+
+function Map:collides(tile)
+   -- checks if a tile value passed in as a parameter is collidable or not
+   local collidables = {
+      TILE_BRICK, JUMP_BLOCK, JUMP_BLOCK_HIT,
+      MUSHROOM_TOP, MUSHROOM_BOTTOM
+   }
+   -- create a table containing all of the values for solid tiles
+
+   -- iterate through them using a for loop to try and match the input tile to each one
+   for _, v in ipairs(collidables) do
+      if tile.id == v then
+         -- if the tile matches a value in the table, return true
+         return true
+      end
+   end
+   -- if we get to the end of the for loop without any matches, return false
+   return false
+end
+
+
+function Map:tileAt(x, y)
+   -- finds what kind of tile is at a particular x/y pixel location on the map
+   return {
+      x = math.floor(x / self.tileWidth) + 1,
+      y = math.floor(y / self.tileHeight) + 1,
+      id = self:getTile(math.floor(x / self.tileWidth) + 1, math.floor(y / self.tileHeight) + 1)
+  }
+  -- returns a table of the tile info containing the tile id and the tilegrid coordinates
+end
+
+function Map:setTile(x, y, tile)
+   self.tiles[(y - 1) * self.mapWidth + x] = tile
+end
+
+function Map:getTile(x, y)
+   return self.tiles[(y - 1) * self.mapWidth + x]
+end
+
+function Map:placeCloud(x)
+   local cloudY = math.random(self.mapHeight / 2 - 6) -- random height at least 6 tiles up
+   self:setTile(x, cloudY, CLOUD_LEFT)
+   self:setTile(x + 1, cloudY, CLOUD_RIGHT)
+end
+
+function Map:placeMushroom(x)
+   self:setTile(x, self.mapHeight / 2 - 2, MUSHROOM_TOP)
+   self:setTile(x, self.mapHeight / 2 - 1, MUSHROOM_BOTTOM)
+   self:placeGround(x)
+   self:placeGround(x + 1)
+end
+
+function Map:placeBush(x)
+   local bushY = self.mapHeight / 2 - 1
+   self:setTile(x, bushY, BUSH_LEFT)
+   self:setTile(x + 1, bushY, BUSH_RIGHT)
+   self:placeGround(x)
+   self:placeGround(x + 1)
+end
+
+function Map:placeGround(x)
+   for y = self.mapHeight / 2, self.mapHeight do
+      self:setTile(x, y, TILE_BRICK)
+   end
+end
+
+function Map:generate()
+   local x = 1
+   while x < self.mapWidth do
+      -- use rng to determine what function to call
+      -- to generate the current column of the map
+      if x < self.mapWidth - 2 and math.random(20) == 1 then
+         -- if we're at least two tiles from the edge
+         -- there's a 1 in 20 chance of generating a cloud
+         self:placeCloud(x)
+      end
+      if math.random(20) == 1 then
+         -- 1 in 20 chance of mushroom
+         self:placeMushroom(x)
+      elseif math.random(10) == 1 and x < self.mapWidth - 3 then
+         -- else a 1 in 10 chance of a bush if we're a safe distance from the edge
+         self:placeBush(x)
+         x = x + 1
+      else
+         if math.random(15) == 1 then
+            -- 1 in 15 chance of creating a block, if th
+            self:setTile(x, self.mapHeight / 2 - 4, JUMP_BLOCK)
+         end
+      end
+      self:placeGround(x)
+      x = x + 1
+   end
+end
+
+function Map:update(dt)
+   local playerCenteredCamX = self.player.x - VIRTUAL_WIDTH / 2
+   self.camX = clamp(playerCenteredCamX, 0, self.mapWidthPixels - VIRTUAL_WIDTH)
+   self.camX = math.floor(self.camX)
+   self.player:update(dt)
+end
+
+function Map:render()
+   for y = 1, self.mapHeight do
+      for x = 1, self.mapWidth do
+         local value = self:getTile(x, y)
+         if value >= 0 then
+            love.graphics.draw(self.spritesheet, self.tileSprites[value], (x - 1) * self.tileWidth, (y - 1) * self.tileHeight)
+         end
+      end
+   end
+   self.player:render()
+end
